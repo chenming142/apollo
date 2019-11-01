7db827bb6599db82b74ea2854ec1a008e4bb1fb8
chenm <Kelvin_chen@aiagain.com>
2019-11-01 10:13:43 +0800

    调整 main-aside-list组件样式

--- src/components/main-aside-list.vue
+++ src/components/main-aside-list.vue
@@ -1,30 +1,57 @@
 <template>
   <div class="main-aside-list">
-    <div v-for="item in wechats" :key="item.personalid">
-      <span v-text="item.getNickname()"></span>
-    </div>
+    <ul v-if="wechats && wechats.length > 0">
+      <li v-for="item in wechats" :key="item.personalid" class="personal">
+          <div class="content">
+            <span v-text="item.personalid"></span> - <span v-text="item.getNickname()"></span>
+          </div>
+
+          <div class="children">
+            <ul class="friend" v-if="item.getFriendList() && item.getFriendList().length > 0">
+              <li v-for="friend in item.getFriendList()">
+                <span v-text="friend.friendid"></span> - <span v-text="friend.getNickname()"></span>
+              </li>
+            </ul>
+            <ul class="friend" v-else><li>无好友</li></ul> 
+
+            <ul class="chatroom" v-if="item.getChatroomList() && item.getChatroomList().length > 0">
+              <li v-for="chatroom in item.getChatroomList()">
+                <span v-text="chatroom.clusterid"></span> - <span v-text="chatroom.getNickname()"></span>
+              </li>
+            </ul>
+            <ul class="chatroom" v-else><li>无群</li></ul>
+
+            <ul class="recent" v-if="item.getRecentList() && item.getRecentList().length > 0">
+              <li v-for="recent in item.getRecentList()">
+                <span v-text="recent.getUniqKey()"></span>
+              </li>
+            </ul>
+            <ul class="recent" v-else><li>无好友</li></ul>
+          </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  import WechatFactory from '../model/wechat';
+  import { WechatFlyweightFactory } from '../model/wechat';
   export default {
-    data () {
-      return {
-
-      }
-    },
     computed: {
       wechats () {
-        return WechatFactory.getWechats();
+        return WechatFlyweightFactory.getWechats();
       }
-    },
-    created () {
-      console.log('\n- components.main-aside-list -------------');
     }
   }
 </script>
 
 <style>
-  .main-aside-list {}
+ul {list-style: disc;padding-left: 40px;}
+li {display: list-item; list-style: none;}
+
+li.personal {text-align: left;}
+li.personal .content { border-bottom: 1px solid gray; padding: 10px 0; margin: 2px 0;}
+
+li.personal div.children { overflow: hidden;}
+li.personal div.children ul {display: block;width: 33.33%;float: left;margin: 0;padding:0;}
+li.personal div.children ul li {padding: 8px 0;}
 </style>
